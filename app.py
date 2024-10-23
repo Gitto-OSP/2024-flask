8b6f48bdbb2563060a400a59dc4c39b395af342c
kkys00 <1903020@ewhain.net>
2024-10-23 23:17:23 +0900

    reg_items -> submit_item_result 간단하게 화면에 나타내기

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sys
 
 application = Flask(__name__)
@@ -47,5 +47,27 @@ def view_regreviews():
 def view_signin():
     return render_template("signin.html")
 
+@application.route("/submit_item_post", methods=['POST'])
+def reg_item_submit_post():
+    form_data = request.form
+    print("POST로 수신된 데이터:")
+    for key, value in form_data.items():
+        print(f"{key}: {value}")
+    print(form_data.getlist('tradeRegions'))
+    
+    image_file=request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
+    data=request.form
+    return render_template("submit_item_result.html", data=data,  img_path="static/images/{}".format(image_file.filename))
+
+@application.route("/submit_items")
+def reg_items_submit():
+    name=request.args.get("name")
+    seller=request.args.get("seller")
+    price=request.args.get("price")
+    addr=request.args.getlist("tradeRegions")
+    status=request.args.get("choice")
+    print(name, seller, addr, price, status)
+
 if __name__ == "__main__":
-    application.run(host='0.0.0.0', debug=Ture)
+    application.run(host='0.0.0.0', debug=True)
