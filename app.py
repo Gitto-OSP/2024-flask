a48bfd5e0699273a7d8fc78bb5677555f78b9809
us2xince <155171017+uoehisx@users.noreply.github.com>
2024-11-26 00:39:25 +0900

    week12 assignment 1,2

--- app.py
+++ app.py
@@ -41,7 +41,29 @@ def view_list():
 
 @application.route("/review")
 def view_review():
-    return render_template("review.html")
+    page=request.args.get("page",0,type=int)
+    per_page=10
+    per_row=5
+    row_count = int(per_page/per_row)
+    start_idx = per_page*page
+    end_idx = per_page*(page+1)
+    data = DB.get_reviews() #read the table
+    review_counts = tot_count = len(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    
+    for i in range(row_count): #last row
+        if(i == row_count-1) and (tot_count%per_row!=0):
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:])
+        else:
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:(i+1)*per_row])
+    return render_template("review.html", 
+                           datas=data.items(), 
+                           row1 = locals()['data_0'].items(),
+                           row2 = locals()['data_1'].items(),
+                           limit=per_page,
+                           page = page,
+                           page_count = int((review_counts/per_page)+1),
+                           total=review_counts)
 
 @application.route("/fleamarket")
 def view_fleamarket():
@@ -148,10 +170,18 @@ def view_regGroupPurchase():
 @application.route("/reg_brand")
 def view_regbrand():
     return render_template("reg_brand.html")
-
-@application.route("/reg_reviews")
-def view_regreviews():
-    return render_template("reg_reviews.html")
+#리뷰쓰기
+@application.route("/reg_review_init/<name>/")
+def reg_review_init(name):
+    return render_template("reg_reviews.html",name=name)
+#등록된 리뷰 DB 등록
+@application.route("/reg_review",methods=['POST'])
+def reg_review():
+    data=request.form
+    image_file=request.files["file"]
+    image_file.save("static/images/{}".format(image_file.filename))
+    DB.reg_review(data,image_file.filename)
+    return redirect(url_for('view_review'))
 
 @application.route('/specificReview')
 def specificReview():
@@ -206,6 +236,23 @@ def view_item_detail(name):
     data=DB.get_item_byname(str(name))
     print("####data:",data)
     return render_template("submit_item_result.html",name=name,data=data)
+#리뷰 상세조회
+
+@application.route("/submit_reviews")
+def reg_reviews_submit():
+    name=request.args.get("name")
+    writer=request.args.get("writer")
+    title=request.args.get("title")
+    rate=request.args.getlist("rate")
+    review=request.args.get("review")
+    print(name, writer,title,rate,review)
+
+@application.route("/info_review/<name>/")
+def view_review_detail(name):
+    print("###name:",name)
+    data=DB.get_review_byname(str(name))
+    print("####data:",data)
+    return render_template("specificReview.html",name=name,data=data)
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
