d8f821fe7a8e9ec745992e80b881b04fe4413381
ksh04 <ksh04@ewha.ac.kr>
2024-12-05 22:55:16 +0900

    시즌 상세페이지

--- database.py
+++ database.py
@@ -65,6 +65,16 @@ class DBhandler:
                 target_value=res.val()
         return target_value
     
+    def get_booth_byname(self,name):
+        items=self.db.child("season").get()
+        target_value=""
+        print("###########",name)
+        for res in items.each():
+            key_value = res.key()
+            if key_value==name:
+                target_value=res.val()
+        return target_value
+    
     def get_gp_byname(self,name):
         items=self.db.child("gp_item").get()
         target_value=""
@@ -202,13 +212,15 @@ class DBhandler:
             for res in bookmarks.each():
                 if res.val().get("interested") == "Y":
                     item_data = self.db.child("item").child(res.key()).get().val()
-                    item = {
-                        "id": res.key(),  # ID
-                        "img_path": item_data.get("img_path", ""),  # 이미지 경로
-                        "tradeRegions": item_data.get("tradeRegions", ""),  # 거래 지역
-                        "price": item_data.get("price", 0)  # 가격
-                    }
-                    linked_items.append(item)
+                    if item_data:  # item_data가 None이 아닌 경우만 처리
+                        img_list = item_data.get("img_path", [])
+                        item = {
+                            "id": res.key(),  # ID
+                            "img_path": img_list,  # 첫 번째 이미지 경로
+                            "tradeRegions": item_data.get("tradeRegions", ""),  # 거래 지역
+                            "price": item_data.get("price", 0)  # 가격
+                        }
+                        linked_items.append(item)
         return linked_items or []
     
     #공동구매
