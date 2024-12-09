05c3bf9f6135eadf4a42da6f5576ce9da73425e3
SominLee <96673257+cottonlee331@users.noreply.github.com>
2024-12-09 12:08:02 +0900

    feat: 네비게이션 바 프로필사진 연결

--- app.py
+++ app.py
@@ -167,6 +167,7 @@ def view_login_user():
     pw_hash = hashlib.sha256(pw.encode('utf-8')).hexdigest()    # db에 저장된 비밀번호 해시값으로 비교위한 해시값 생성
     if DB.find_user(id_,pw_hash):
         session['id']=id_    # session에 id 정보 삽입
+        session['prof_img']=DB.get_userInfo(id_,'profile_image')
         return redirect(url_for('view_list'))
     else:
         flash("잘못된 아이디 혹은 비밀번호를 입력하셨습니다.")    #db에 매칭 정보가 없으면 플래시 메세지 생성
